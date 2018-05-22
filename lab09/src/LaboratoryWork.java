2355b13a4667f6a3f8a528c7f56602a15352376f
Anton V. Kotliarenko <antdenver@gmail.com>
2018-05-22 16:22:06 +0200

    LAB9 commit 03

--- lab09/src/LaboratoryWork.java
+++ lab09/src/LaboratoryWork.java
@@ -1,4 +1,11 @@
+
 public class LaboratoryWork {
+
+    /* Отфильтровать массив так, чтобы оставить только четные элементы. Для проверки четности можно использовать операцию "остаток от деления" - %
+            3 % 2 == 1;
+            6 % 2 == 0;
+    Исходящий массив, для простоты, должен быть того же размера, что и входящий. Например, для {4, 3, 5, 6, 7, 9} -> {4, 6, 0, 0, 0, 0}
+    */
 
     public int[] filterArrayCuttingLength(int[] input) {
         int length = input.length;
@@ -16,28 +23,25 @@ public class LaboratoryWork {
 
             if (arrayItem % 2 == 0) {
                 output[counter2] = arrayItem;
-                Logging.LOG_INFO("output " +counter2+ " ==>"+output[counter2]);
+                Logging.LOG_INFO("output array cutting length: " + counter2 + " ==>" + output[counter2]);
                 counter2++;
             }
         }
-
         return output;
     }
 
-    public int[] filterArray(int[] input)
-    {
-        return input;
-
+    public int[] filterArray(int[] input) {
+        int[] numbsReturn = new int[input.length];
+        int counterNumbs = 0;
+        for (int element : input
+                ) {
+            if (element % 2 == 0) {
+                Logging.LOG_INFO("output array: [" + counterNumbs + "] even number " + element);
+                numbsReturn[counterNumbs] = element;
+                counterNumbs++;
+            }
+        }
+        return numbsReturn;
     }
-
-
-    // Logging.LOG_INFO("array size is - " + length);
-
-
-   /* Отфильтровать массив так, чтобы оставить только четные элементы. Для проверки четности можно использовать операцию "остаток от деления" - %
-            3 % 2 == 1;
-6 % 2 == 0;
-    Исходящий массив, для простоты, должен быть того же размера, что и входящий. Например, для {4, 3, 5, 6, 7, 9} -> {4, 6, 0, 0, 0, 0}
-    */
 
 }
