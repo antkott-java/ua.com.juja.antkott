302274ed3f256d6c62816d641c09c560d4e84d5a
Anton V. Kotliarenko <antdenver@gmail.com>
2018-05-30 00:17:49 +0200

    ANTKOTT LAB12a commit 02

--- lab12a/src/ua/com/juja/core/antkott/LaboratoryWork.java
+++ lab12a/src/ua/com/juja/core/antkott/LaboratoryWork.java
@@ -3,33 +3,32 @@ package ua.com.juja.core.antkott;
 import static lab.utils.Logging.LOG_INFO;
 
 public class LaboratoryWork {
+    public static final int ERROR_CODE = -1;
 
-    /*
-
-     */
-
-
-    public int findUnpairedNumber(int[] array) {
-        int result = -1;
+    public int findUnpairedNumber(int array[]) {
+        int result = ERROR_CODE;
         for (int i = 0; i < array.length; i++) {
-            int currentItem = array[i];
-            boolean pairedNumberFound = false;
-
-            for (int j = 0; j < array.length; j++) {
-                if (j == i) {
-                    continue;
-                } else if (currentItem == array[j]) {
-                    pairedNumberFound = true;
-                    LOG_INFO("found paired for " + currentItem + " this is " + array[j]);
-
-                }
-            }
-            if (!pairedNumberFound) {
+            if (!isPairedNumberFound(array, i)) {
                 result = i;
             }
         }
         LOG_INFO("found Unpaired number index = " + result);
         return result;
+    }
+
+    private boolean isPairedNumberFound(int[] array, int currentIndex) {
+        int currentItem = array[currentIndex];
+
+        for (int j = 0; j < array.length; j++) {
+            if (j == currentIndex) {
+                continue;
+            }
+            if (currentItem == array[j]) {
+                LOG_INFO("found paired for " + currentItem + " this is " + array[j]);
+                return true;
+            }
+        }
+        return false;
     }
 
 }
