79a6e9a3405676f4560e065f49d3649687eb083e
Anton V. Kotliarenko <antdenver@gmail.com>
2018-06-08 17:39:49 +0200

    ANTKOTT Lab18 commit 01

--- lab18/src/ua/com/juja/core/antkott/ParserOrigin.java
+++ lab18/src/ua/com/juja/core/antkott/ParserOrigin.java
@@ -2,36 +2,30 @@ package ua.com.juja.core.antkott;
 
 import static ua.com.juja.core.antkott.utils.Logging.logInfo;
 
-public class Parser {
+public class ParserOrigin {
     public static void main(String[] args) {
         System.out.println(eval("123"));
         System.out.println(eval("2*3"));
         System.out.println(eval("2*(1+3)"));
         System.out.println(eval("1+(5-2*(13/6))"));
     }
-    
-    
+
     public static int eval(String expr) {
         return eval(expr, 0, expr.length());
     }
 
     private static int eval(String expr, int from, int to) {
-
-        int length = expr.length();
-        logInfo("input expr: " + expr + " from:" + from + " to:" + to + getExpr(expr, from, to)+" length:"+length);
-        //from=length;
-        //if (to==length) to--;
-
-        if (expr.charAt(to) == ')') {
-            logInfo("if ')': " +  getExpr(expr, from+1, to-1));
-            return eval(expr, from + 1, to - 1);
+        logInfo("input expr: " + expr + " from:" + from + " to:" + to + getExpr(expr, from, to));
+        if (expr.charAt(from) == '(') {
+            logInfo("if '(': " +  getExpr(expr, from+1, to-1));
+            return eval(expr, from + 1, to-1);
         } else {
             logInfo("else '(': " +   getExpr(expr, from, to));
-            int pos = to;
-            while (pos < from) {
+            int pos = from;
+            while (pos < to) {
                 if (Character.isDigit(expr.charAt(pos))) {
-                    pos--;
-                    logInfo(" $"+expr.charAt(pos)+"$ pos++: " + pos);
+                    pos++;
+                    logInfo("pos++: " + pos);
                 } else {
                     logInfo("leftOperand: " + (expr.substring(from, pos)));
                     int leftOperand = Integer.valueOf(expr.substring(from, pos));
--- lab18/test/ParserTest.java
+++ lab18/test/ParserTest.java
@@ -4,7 +4,7 @@ public class ParserTest {
     public static void main(String[] args) {
         //System.out.println(">> 123 = " + Parser.eval("123"));
         //System.out.println(">> 2*3 = " + Parser.eval("2*3"));
-        System.out.println(">> (1+3)*2 = " + Parser.eval("(1+3)*2"));
-        //System.out.println(">> ((13/6)*2-5)+1 = " + Parser.eval("((13/6)*2-5)+1"));
+        //System.out.println(">> (1+3)*2 = " + Parser.eval("(1+3)*2"));
+        System.out.println(">> ((13/6)*2-5)+1 = " + Parser.eval("((13/6)*2-5)+1"));
     }
 }
