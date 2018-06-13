3a866a58b8e726ac9accf14fa7127e8ed736caa7
Anton V. Kotliarenko <antdenver@gmail.com>
2018-06-13 23:50:45 +0200

    ANKOTT Lab18 commit 03

--- lab18/src/ua/com/juja/core/antkott/Parser.java
+++ lab18/src/ua/com/juja/core/antkott/Parser.java
@@ -9,34 +9,47 @@ public class Parser {
         System.out.println(eval("2*(1+3)"));
         System.out.println(eval("1+(5-2*(13/6))"));
     }
-    
-    
+
+
     public static int eval(String expr) {
-        return eval(expr, 0, expr.length());
+        return eval(expr, 0, expr.length()-1);
     }
 
     private static int eval(String expr, int from, int to) {
 
         int length = expr.length();
-        logInfo("input expr: " + expr + " from:" + from + " to:" + to + getExpr(expr, from, to)+" length:"+length);
+        //if (to == length) to--;
+        logInfo("INPUT " +getExpr(expr, from, to));
+        logInfo("(from:to)=(" +from+":"+ to+")");
         //from=length;
-        //if (to==length) to--;
 
+        logInfo(" if expr.charAt(to:"+to+")==) (" + expr.charAt(to)+"), "+(expr.charAt(to) == ')'));
         if (expr.charAt(to) == ')') {
-            logInfo("if ')': " +  getExpr(expr, from+1, to-1));
+            logInfo(" if ')': " + getExpr(expr, from + 1, to - 1));
             return eval(expr, from + 1, to - 1);
         } else {
-            logInfo("else '(': " +   getExpr(expr, from, to));
+//            logInfo(" else ')': " + getExpr(expr, from, to));
             int pos = to;
-            while (pos < from) {
+            logInfo(" (pos>from) " + pos + ">" + from + " = "+(pos > from));
+            while (pos > from) {
+                logInfo("  start while: expr.charAt("+pos+"):" + expr.charAt(pos));
                 if (Character.isDigit(expr.charAt(pos))) {
+                    logInfo("   character.isDigit("+pos+"):" + expr.charAt(pos)+" pos--");
                     pos--;
-                    logInfo(" $"+expr.charAt(pos)+"$ pos++: " + pos);
                 } else {
-                    logInfo("leftOperand: " + (expr.substring(from, pos)));
-                    int leftOperand = Integer.valueOf(expr.substring(from, pos));
+                    logInfo("   pos:from:to "+pos+":"+from+":"+to);
+                    logInfo("   Character.isNOTDigit("+pos+"):" + expr.charAt(pos));
+//                    logInfo("leftOperand: " +  getExpr(expr, from, pos));
+//                    int leftOperand = Integer.valueOf(expr.substring(from, pos));
+                    logInfo("   rightOperand: Integer.valueOf: " +  getExpr(expr, pos+1, pos+2));
+                    int rightOperand = Integer.valueOf(expr.substring(pos+1, pos+2));
+                    logInfo("   operation: expr.charAt("+pos+"):" + expr.charAt(pos));
                     char operation = expr.charAt(pos);
-                    int rightOperand = eval(expr, pos + 1, to);
+                    logInfo("   leftOperand: run another eval" +  getExpr(expr, from, pos-1));
+                    int leftOperand = eval(expr, from, pos-1);
+
+//                    int rightOperand = eval(expr, pos + 1, to);
+
                     logInfo("operation:" + operation + " expr:" + expr + " chartAt(" + pos + ") is:" + operation);
                     switch (operation) {
                         case '+':
@@ -50,11 +63,12 @@ public class Parser {
                     }
                 }
             }
-            return Integer.valueOf(expr.substring(from, to));
+            logInfo("return Integer.valueOf " + getExpr(expr, from, to+1));
+            return Integer.valueOf(expr.substring(from, to+1));
         }
     }
 
     private static String getExpr(String expr, int from, int to) {
-        return " ["+expr.substring(from, to)+"] ";
+        return "("+from+":"+to+"):" + expr.substring(from, to) + ":";
     }
 }
--- lab18/test/ParserTest.java
+++ lab18/test/ParserTest.java
@@ -2,9 +2,9 @@ import ua.com.juja.core.antkott.Parser;
 
 public class ParserTest {
     public static void main(String[] args) {
-        //System.out.println(">> 123 = " + Parser.eval("123"));
-        //System.out.println(">> 2*3 = " + Parser.eval("2*3"));
-        //System.out.println(">> (1+3)*2 = " + Parser.eval("(1+3)*2"));
+//        System.out.println(">> 123 = " + Parser.eval("123"));
+//        System.out.println(">> 2*3 = " + Parser.eval("2*3"));
+//        System.out.println(">> (1+3)*2 = " + Parser.eval("(1+3)*2"));
         System.out.println(">> ((13/6)*2-5)+1 = " + Parser.eval("((13/6)*2-5)+1"));
     }
 }
