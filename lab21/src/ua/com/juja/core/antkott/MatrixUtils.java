2e4d6aca72262dee9719903a07d7586009e67f9d
Anton V. Kotliarenko <antdenver@gmail.com>
2018-06-19 19:59:41 +0200

    ANTKOTT Lab21 commit 00

---
+++ lab21/src/ua/com/juja/core/antkott/MatrixUtils.java
@@ -0,0 +1,8 @@
+package ua.com.juja.core.antkott;
+
+public class MatrixUtils {
+    public static int[][] mul(int[][] fst, int[][] snd) {
+        /*BODY*/
+        return  fst;
+    }
+}
---
+++ lab21/test/Test.java
@@ -0,0 +1,23 @@
+import ua.com.juja.core.antkott.MatrixUtils;
+
+public class Test {
+    
+    @org.junit.jupiter.api.Test
+    public static void main(String[] args) {
+        int[][] a = {{10}};
+        int[][] b = {{100}};
+        int[][] c = MatrixUtils.mul(a, b);
+
+        if (c.length != 1) {
+            throw new AssertionError();
+        }
+        if (c[0].length != 1) {
+            throw new AssertionError();
+        }
+        if (c[0][0] != a[0][0] * b[0][0]) {
+            throw new AssertionError();
+        }
+
+        System.out.print("OK");
+    }
+}
